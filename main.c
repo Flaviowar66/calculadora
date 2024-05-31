6389fe3c88602cd761bcf393d44899deb423e923
Flaviowar66 <flavioantunes457@gmail.com>
2024-05-31 14:18:03 -0300

    criei a soma

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-void main() {
-
-    printf("Hello world\n");
+int soma(int n1, int n2) {
+    return n1+n2;
+   
 }
+
+int main(){
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    int resultado= soma(num1, num2);
+    printf("%d", resultado);
+
+
+
+
+}
+
